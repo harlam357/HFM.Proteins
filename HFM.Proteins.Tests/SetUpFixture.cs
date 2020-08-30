0ee0c3302d50d2ea78a690a89643b84c58845ed9
harlam357 <harlam357@gmail.com>
2020-08-30 18:03:41 -0500

    initial commit

---
+++ HFM.Proteins.Tests/SetUpFixture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace HFM.Proteins
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        [OneTimeSetUp]
+        public void SetEnvironmentCurrentDirectory()
+        {
+            Environment.CurrentDirectory = TestContext.CurrentContext.TestDirectory;
+        }
+    }
+}
